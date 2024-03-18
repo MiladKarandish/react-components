589519d6b176a50fdb663c06cae43e8c714b6a66
Milad Karandish <80484107+MiladKarandish@users.noreply.github.com>
2024-03-18 23:21:18 +0330

    working on the dropdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
 "use client";
 
+import Dropdown from "@/components/Dropdown";
 import OTP from "../components/Otp";
+import { useState } from "react";
 
 export default function Home() {
+  const [selectedOption, setSelectedOption] = useState<string>(null!);
+
+  const handleSelect = (option: string) => {
+    setSelectedOption(option);
+    console.log("Selected option:", option);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <OTP onReachEnd={(value) => console.log(value)} />
+
+      <Dropdown
+        options={["Option 1", "Option 2", "Option 3"]} // Pass options as an array of strings
+        onSelect={handleSelect} // Pass the callback function to handle selection
+      />
     </main>
   );
 }
--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,7 +1,52 @@
-export interface Props {}
+import React, { useState, useMemo } from "react";
+import "./style.css";
 
-const Dropdown = ({}: Props) => {
-  return <div>Dropdown</div>;
+interface DropdownProps {
+  options: string[];
+  onSelect: (option: string) => void;
+}
+
+const Dropdown: React.FC<DropdownProps> = ({ options, onSelect }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSelect = (option: string) => {
+    setSelectedOption(option);
+    onSelect(option);
+    setIsOpen(false);
+  };
+
+  const dropdownItems = useMemo(
+    () =>
+      options.map((option, index) => (
+        <div
+          key={index}
+          className="dropdown-item"
+          onClick={() => handleSelect(option)}
+        >
+          {option}
+        </div>
+      )),
+    [options, handleSelect]
+  );
+
+  return (
+    <div className="dropdown-container">
+      <div
+        className={`dropdown-header ${isOpen ? "open" : ""}`}
+        onClick={toggleDropdown}
+      >
+        {selectedOption || "Select an option"}
+      </div>
+      <div className={`dropdown-menu ${isOpen ? "show" : ""}`}>
+        {dropdownItems}
+      </div>
+    </div>
+  );
 };
 
-export default Dropdown;
+export default React.memo(Dropdown);
